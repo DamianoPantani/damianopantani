1839e2083bf225a203acf1174b8f5621bb42dacc
szafulskid <D.szafulski@tt.com.pl>
2016-11-04 15:06:16 +0100

    First commit

---
+++ before_load.js
@@ -0,0 +1,21 @@
+var cookieColor = getCookie('main-color');
+changeColor(cookieColor ? cookieColor : 180);
+
+function changeColor(color){
+    document.documentElement.style.setProperty('--main-color', color);
+    setCookie('main-color', color);
+}
+
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+function getCookie(key) {
+    var value = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return value === null ? undefined : value[2];
+}
+
+function confL(message) {
+    return window.confirm(message);
+}
