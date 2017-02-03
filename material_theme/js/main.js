d160e2e894186eb6b5d49fca6be0e9837786dcea
szafulskid <D.szafulski@tt.com.pl>
2017-02-03 10:35:49 +0100

    Fixed not working smooth scrolling and tooltips

--- material_theme/js/main.js
+++ material_theme/js/main.js
@@ -63,7 +63,7 @@ jQuery(function($) {
 	
 	/* Smooth scroll to element */
 	$(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 		  var target = $(this.hash);
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
