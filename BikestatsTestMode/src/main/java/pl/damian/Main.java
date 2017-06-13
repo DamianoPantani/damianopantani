b2bfb65e4b3c848f74b7a12f3db4a6d3dcdc3045
Damian Szafulski <damian.szafulski@origamienergy.com>
2017-06-13 21:29:36 +0200

    Minified js

--- BikestatsTestMode/src/main/java/pl/damian/Main.java
+++ BikestatsTestMode/src/main/java/pl/damian/Main.java
@@ -12,12 +12,14 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.yahoo.platform.yui.compressor.Bootstrap;
+
 public class Main {
 
 	private static final String ALL_JS = "all.js";
 	private static final String TEST_MODE_SUFFIX = "TEST_MODE";
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
 		mergeJsIn("js/");
 		
@@ -47,7 +49,7 @@ public class Main {
 		Desktop.getDesktop().browse(file.toURI());
 	}
 
-	private static void mergeJsIn(String path) throws IOException {
+	private static void mergeJsIn(String path) throws Exception {
 		File merged = new File(path+ALL_JS);
 		merged.delete();
 		merged.createNewFile();
@@ -60,10 +62,19 @@ public class Main {
 		
 		for (File js: fileList){
 			appendToFile(readFile(js), merged.getAbsolutePath());
+			if (js.getName().equals("main.jsmin.js") || js.getName().equals("fw_emoticons.jsmin.js")){
+				js.delete();
+			}
 		}
 	}
 
-	private static List<File> sortScripts(File[] allFiles) {
+	private static File minify(File file) throws Exception {
+		String minifiedFileName = file.getName()+"min.js";
+		Bootstrap.main(new String[]{"--charset", "UTF-8", "-v", "-o", minifiedFileName, file.getPath()});
+		return new File(minifiedFileName);
+	}
+
+	private static List<File> sortScripts(File[] allFiles) throws Exception {
 		List<File> results = new ArrayList<>();
 		for (File f: allFiles){
 			if (f.getName().equals("jquery.min.js")){
@@ -81,8 +92,8 @@ public class Main {
 			}
 		}
 		for (File f: allFiles){
-			if (f.getName().equals("main.js")){
-				results.add(f);
+			if (f.getName().equals("main.js") || f.getName().equals("fw_emoticons.js")){
+				results.add(minify(f));
 			}
 		}
 		return results;
