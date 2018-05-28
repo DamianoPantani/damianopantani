c84b3b84805d28653f59dadef35564c6f4f9ccd7
Damian Szafulski <damian.szafulski@origamienergy.com>
2018-05-28 20:51:28 +0200

    Unified css and jQuery rules for gps wrapper

--- material_theme/js/main.js
+++ material_theme/js/main.js
@@ -167,7 +167,7 @@ function initCounter() {
 function hideGPSTracksOnMainPage(){
 	$('iframe.hidden').detach();
 	$('button:contains("Pokaż trasę GPS")').detach();
-	$('iframe[src*=rwgps], iframe[src*=ridewithgps]').wrap('<div class="gps-wrapper"></div>');
+	$('iframe[src*=route]').wrap('<div class="gps-wrapper"></div>');
 }
 
 function hideCommentsLinkIfZero(){
